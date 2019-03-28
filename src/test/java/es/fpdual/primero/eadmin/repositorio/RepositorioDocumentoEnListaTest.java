a1b292b39e0c728d5fbffbbdadf1586d9e884ecb
formacion <formacion@192.168.136.144>
2019-03-28 14:31:51 +0100

    repositorioDocumentoEnLista

---
+++ src/test/java/es/fpdual/primero/eadmin/repositorio/RepositorioDocumentoEnListaTest.java
@@ -0,0 +1,70 @@
+package es.fpdual.primero.eadmin.repositorio;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.fpdual.primero.eadmin.modelo.AdministracionElectronicaException;
+import es.fpdual.primero.eadmin.modelo.Documento;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class RepositorioDocumentoEnListaTest {
+
+	private RepositorioDocumentoEnLista repositorioDocumento;
+	
+	private Documento documento;
+
+	@Before
+	public void inicializarEnCadaTest() {
+		this.repositorioDocumento = new RepositorioDocumentoEnLista();
+		this.documento = mock(Documento.class);
+	}
+
+	@Test
+	public void deberiaAlmacenarUnNuevoDocumento() {
+
+		when(documento.getNombre()).thenReturn("documento 1");
+		when(documento.getId()).thenReturn(5);
+
+		this.repositorioDocumento.altaDocumento(documento);
+
+		assertTrue(this.repositorioDocumento.obtenerTodosLosDocumento().contains(documento));
+	}
+	
+	@Test (expected = AdministracionElectronicaException.class)
+	public void deberiaLanzarUnaExcepcionAlAlmacenarUnDocumentoYaExistente() {
+		
+		when(documento.getNombre()).thenReturn("documento 1");
+		when(documento.getId()).thenReturn(5);
+		
+		this.repositorioDocumento.altaDocumento(documento);
+		this.repositorioDocumento.altaDocumento(documento);
+	}
+	
+	@Test
+	public void deberiaModificarUnDocumento() {
+		
+		Documento documentoAlmacenado = new Documento(20, "doc1", null, null, null);
+		Documento documentoModificado = new Documento(20, "doc2", null, null, null);
+		
+//		when(documento.getNombre()).thenReturn("documento 1");
+//		when(documento.getId()).thenReturn(5);	
+//		when(documentoModificado.getNombre()).thenReturn("documento modificado");
+//		when(documentoModificado.getId()).thenReturn(5);
+		
+		this.repositorioDocumento.altaDocumento(documentoAlmacenado);
+		this.repositorioDocumento.modificarDocumento(documentoModificado);
+		
+		assertEquals("doc2", this.repositorioDocumento.obtenerTodosLosDocumento().get(0).getNombre());
+	}
+	
+	@Test (expected = AdministracionElectronicaException.class)
+	public void deberiaLanzarUnaExcepcionAlModificarUnDocumentoNoExistente() {
+		
+		final Documento documentoModificado = mock(Documento.class);
+		
+		this.repositorioDocumento.modificarDocumento(documento);
+	}
+}
