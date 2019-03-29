abc2ead35dd5d1154670e94456653bf5a4db070d
formacion <formacion@192.168.136.144>
2019-03-29 14:21:13 +0100

    nuevo commit (Controladores, DocumentoRequest...)

--- src/main/java/es/fpdual/primero/eadmin/controlador/ControladorDocumento.java
+++ src/main/java/es/fpdual/primero/eadmin/controlador/ControladorDocumento.java
@@ -1,9 +1,39 @@
 package es.fpdual.primero.eadmin.controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.fpdual.primero.eadmin.modelo.Documento;
+import es.fpdual.primero.eadmin.servicio.ServicioDocumento;
 
 @RestController
 public class ControladorDocumento {
 
+	ServicioDocumento servicioDocumento;
+
+	@Autowired
+	public ControladorDocumento(ServicioDocumento servicioDocumento) {
+		this.servicioDocumento = servicioDocumento;
+	}
 	
+	@PostMapping ("/documentos")
+	public Documento altaDocumento(@RequestBody DocumentoRequest documentoRequest) {
+		
+		final Documento documentoAlInsertar = DocumentoRequestMapper.toDocumento(documentoRequest);
+		return this.servicioDocumento.altaDocumento(documentoAlInsertar);
+	}
+	
+	@GetMapping
+	public void modificarDocumento(@RequestBody Documento documento) {
+		
+	}
+	
+	@DeleteMapping
+	public void eliminarDocumento(@RequestBody Documento documento) {
+		
+	}
 }
---
+++ src/main/java/es/fpdual/primero/eadmin/controlador/DocumentoRequest.java
@@ -0,0 +1,43 @@
+package es.fpdual.primero.eadmin.controlador;
+
+import java.util.Date;
+
+public class DocumentoRequest {
+	
+	private final int id;
+	private final String nombre;
+	private final String usuario;
+	private final Date fechaCreacion;
+	private final String tipoDocumento;
+	
+	
+	public DocumentoRequest(int id, String nombre, String usuario, Date fechaCreacion, String tipoDocumento) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.usuario = usuario;
+		this.fechaCreacion = fechaCreacion;
+		this.tipoDocumento = tipoDocumento;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+}
