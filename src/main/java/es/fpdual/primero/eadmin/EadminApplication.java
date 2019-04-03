e2d8ff1cc8ab2856f82709d969c1c041b9999736
formacion <formacion@192.168.136.144>
2019-04-03 08:13:34 +0200

    un nuevo comentario

--- src/main/java/es/fpdual/primero/eadmin/EadminApplication.java
+++ src/main/java/es/fpdual/primero/eadmin/EadminApplication.java
@@ -1,7 +1,7 @@
 package es.fpdual.primero.eadmin;
 
 
-import java.util.Date;
+//import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
